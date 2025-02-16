e14f5d318bad86786efe6b8871b5f0a710a889da
Rafael Fernandes <rf7223216@gmail.com>
2025-02-16 18:30:51 -0300

    muita coisa

--- assets/js/actionButton.js
+++ assets/js/actionButton.js
@@ -5,27 +5,29 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     const backToTopButton = document.getElementById('backToTop');
-    const whatsappButton = document.getElementById('whatsappButton');
+    const downloadButton = document.getElementById('downloadButton');
 
-    // Função para exibir ou esconder os botões ao rolar a página
     function toggleButtons() {
         const scrollPosition = window.scrollY;
-        if (scrollPosition > 100) {
+        
+        // Ajuste aqui o valor do scroll necessário para exibir os botões
+        if (scrollPosition > 0) { 
             backToTopButton.style.display = 'flex';
-            whatsappButton.style.display = 'flex';
+            downloadButton.style.display = 'flex';
             setTimeout(() => {
                 backToTopButton.style.opacity = '1';
-                whatsappButton.style.opacity = '1';
+                downloadButton.style.opacity = '1';
             }, 10);
         } else {
             backToTopButton.style.opacity = '0';
-            whatsappButton.style.opacity = '0';
+            downloadButton.style.opacity = '0';
             setTimeout(() => {
                 backToTopButton.style.display = 'none';
-                whatsappButton.style.display = 'none';
+                downloadButton.style.display = 'none';
             }, 500);
         }
     }
+    
 
     // Adiciona o evento de scroll para exibir ou esconder os botões
     document.addEventListener('scroll', toggleButtons);
@@ -35,15 +37,21 @@ document.addEventListener('DOMContentLoaded', function () {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 
-    // Ação ao clicar no botão do WhatsApp
-    whatsappButton.addEventListener('click', function () {
-        window.location.href = 'https://wa.me/5599999999999';
-    });    
+    downloadButton.addEventListener('click', function () {
+        const link = document.createElement('a');
+        link.href = '../../assets/documents/Currículo-Rafa.pdf';
+        link.download = 'Currículo-Rafa.pdf'; 
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+        
 
    document.getElementById("cta-sobre").addEventListener ("click", function(){
         document.getElementById("sobre").scrollIntoView({
             behavior: "smooth"
         });
    });
-    
+
+     
 });
--- assets/js/header.js
+++ assets/js/header.js
@@ -1,9 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Elementos de controle do menu
+    // Elementos do menu e header
     const menuToggle = document.getElementById('menuToggle');
     const menuAberto = document.querySelector('.menu-aberto');
     const navLinks = document.querySelectorAll('.nav-menu a');
     const body = document.body;
+    const header = document.querySelector('header'); // Pegue o header
+    const menu = document.querySelector('.nav-menu'); // Pegue o menu
+
+    // Função para exibir/esconder o menu ao rolar a página
+    function toggleMenuOnScroll() {
+        const scrollPosition = window.scrollY;
+        const inicio = document.getElementById('inicio');
+
+        if (scrollPosition > inicio.clientHeight - 50) { 
+            header.classList.add('visible');
+            menu.classList.add('visible');
+        } else {
+            header.classList.remove('visible');
+            menu.classList.remove('visible');
+        }
+    }
+
+    // Detecta scroll e aplica mudanças
+    document.addEventListener('scroll', toggleMenuOnScroll);
 
     // Abre/fecha o menu ao mudar o estado do checkbox
     menuToggle.addEventListener('change', function () {
