d29530cf8b81026cda4185d515e55a71eefd00de
Rafael Fernandes <rf7223216@gmail.com>
2025-03-07 20:59:09 -0300

    certificates

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -2,21 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const languageSwitch = document.getElementById("language-switch");
     const languageIcon = document.getElementById("language-icon");
 
-    const flagPT = "assets/img/svg/Brasil.svg"; 
-    const flagEN = "assets/img/svg/English.svg"; 
+    const flagPT = "assets/img/svg/Brasil.svg";
+    const flagEN = "assets/img/svg/English.svg";
 
     const translations = {
         "pt": {
             "home": "Início",
             "portfolio": "Portfólio",
             "aboutnav": "Sobre mim",
-            "about": "Olá, me chamo  Rafael",
+            "about": "Olá, me chamo Rafael",
             "speaktome": "Fale comigo",
             "scroll": "Role para baixo",
             "descriptionAbout": "Movido pela curiosidade, busco transformar ideias em experiências visuais cativantes. Este espaço reflete minha paixão pela inovação.",
             "download": "Baixar o Curriculo",
-            "certificates": "Certificados",
-            "descriptionCertificates": "Certificados de cursos e eventos em que participei.",
             "mySkills": "Minhas habilidades",
             "descriptionMySkills": "Conheça as habilidades que possuo e as ferramentas que utilizo.",
             "portuguese": "Português",
@@ -38,9 +36,18 @@ document.addEventListener("DOMContentLoaded", function () {
             "site": "Ir ao Site",
             "code": "Ver Código",
             "contact": "Contato",
-            "credits": "2024 Todos os direitos reservados. Desenvolvido por Raffa."
+            "credits": "2024 Todos os direitos reservados. Desenvolvido por Raffa.",
+            "culturadigital": "Bradesco - Cultura Digital",
+            "letramentodigital": "Bradesco - Letramento Digital",
+            "fundamentosti": "Bradesco - Fundamentos de TI",
+            "sharepoint": "Bradesco - Sharepoint",
+            "designgrafico": "Area Z - Design Gráfico",
+            "aftereffects": "Area Z - After Effects",
+            "uspvemsaber": "USP - Vem Saber",
+            "uspnegociacao": "USP - Negociação Internacional",
+            "certificates": "Certificados",
+            "descriptionCertificates": "Certificados de cursos e eventos em que participei."
         },
-
         "en": {
             "home": "Home",
             "portfolio": "Portfolio",
@@ -50,8 +57,6 @@ document.addEventListener("DOMContentLoaded", function () {
             "scroll": "Scroll down",
             "descriptionAbout": "Driven by curiosity, I strive to turn ideas into captivating visual experiences. This space reflects my passion for innovation.",
             "download": "Download CV",
-            "certificates": "Certificates",
-            "descriptionCertificates": "Certificates from courses and events I have attended.",
             "mySkills": "My skills",
             "descriptionMySkills": "Discover the skills I have and the tools I use.",
             "portuguese": "Portuguese",
@@ -73,9 +78,18 @@ document.addEventListener("DOMContentLoaded", function () {
             "site": "Go to Site",
             "code": "View Code",
             "contact": "Contact",
-            "credits": "2024 All rights reserved. Developed by Raffa."
+            "credits": "2024 All rights reserved. Developed by Raffa.",
+            "culturadigital": "Bradesco - Digital Culture",
+            "letramentodigital": "Bradesco - Digital Literacy",
+            "fundamentosti": "Bradesco - IT Fundamentals",
+            "sharepoint": "Bradesco - Sharepoint",
+            "designgrafico": "Area Z - Graphic Design",
+            "aftereffects": "Area Z - After Effects",
+            "uspvemsaber": "USP - Vem Saber",
+            "uspnegociacao": "USP - International Negotiation",
+            "certificates": "Certificates",
+            "descriptionCertificates": "Certificates of courses and events I have participated in."
         }
-
     };
 
     let currentLang = localStorage.getItem("language") || "pt";
@@ -101,4 +115,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
-
