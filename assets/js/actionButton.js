fc57297847a0e11bafc7f457f408a7df0df627d0
Rafael Fernandes <rf7223216@gmail.com>
2025-03-12 15:55:25 -0300

    design

--- assets/js/actionButton.js
+++ assets/js/actionButton.js
@@ -35,12 +35,11 @@ document.addEventListener('DOMContentLoaded', function () {
             behavior: "smooth"
         });
     });
-    
+
 
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Ajusta para garantir que o site comece no topo ao recarregar
     if (window.location.hash !== '#inicio') {
         window.location.hash = '#inicio';
     }
@@ -48,7 +47,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const backToTopButton = document.getElementById('backToTop');
     const whatsappButton = document.getElementById('whatsappButton');
 
-    // Função para exibir ou esconder os botões ao rolar a página
     function toggleButtons() {
         const scrollPosition = window.scrollY;
         if (scrollPosition > 100) {
@@ -68,16 +66,15 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Adiciona o evento de scroll para exibir ou esconder os botões
     document.addEventListener('scroll', toggleButtons);
 
-    // Ação ao clicar no botão "Voltar ao Topo"
     backToTopButton.addEventListener('click', function () {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 
-    // Ação ao clicar no botão do WhatsApp
     whatsappButton.addEventListener('click', function () {
-        window.location.href = 'https://wa.me/5599999999999';
+        const mensagem = encodeURIComponent("Olá! Gostaria de saber mais sobre seus serviços.");
+        window.location.href = `https://wa.me/5517992632342?text=${mensagem}`;
     });
+
 });
